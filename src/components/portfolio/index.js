761916caeb25df3689d121fbbaed68f462605846
Ozdal Dogru <ozdaldogru@gmail.com>
2024-03-07 23:01:07 -0500

    src/components/portfolio/index.js file coded from scratch

---
+++ src/components/portfolio/index.js
@@ -0,0 +1,148 @@
+
+function Portfolio() {
+  return (
+    <div>
+      <section id="work" className="jobs">
+        <div className="flex-row">
+          <h2 className="section-title secondary-border">Portfolio</h2>
+        </div>
+
+        <div className="job">
+          <div className="job-info">
+            <div className="job-img">
+              <a href="">
+                {" "}
+                <img
+                  src={}
+                  className="my-2"
+                  style={{ width: "100%" }}
+                  alt=""
+                />
+              </a>
+            </div>
+                        <div className="job-text">
+              <h4>Project-1</h4>
+              <p>
+                Project-1 description
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="job">
+          <div className="job-info">
+            <div className="job-img">
+              <a href="">
+                {" "}
+                <img
+                  src={}
+                  className="my-2"
+                  style={{ width: "100%" }}
+                  alt=""
+                />
+              </a>
+            </div>
+            <div className="job-text">
+              <h4>Project-2</h4>
+              <p>
+              Project-2 description
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="job">
+          <div className="job-info">
+            <div className="job-img">
+              <a href="">
+                {" "}
+                <img
+                  src={}
+                  className="my-2"
+                  style={{ width: "100%" }}
+                  alt=""
+                />
+              </a>
+            </div>
+            <div className="job-text">
+              <h4>Project-3</h4>
+              <p>
+              Project-3 description
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="job">
+          <div className="job-info">
+            <div className="job-img">
+              <a href="">
+                {" "}
+                <img
+                  src={}
+                  className="my-2"
+                  style={{ width: "100%" }}
+                  alt=""
+                />
+              </a>
+            </div>
+            <div className="job-text">
+              <h4>Project-4</h4>
+              <p>
+              Project-4 description
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="job">
+          <div className="job-info">
+            <div className="job-img">
+              <a href="">
+                {" "}
+                <img
+                  src={}
+                  className="my-2"
+                  style={{ width: "100%" }}
+                  alt=""
+                />
+              </a>
+            </div>
+            <div className="job-text">
+              <h4>Project-5</h4>
+              <p>
+              Project-5 description
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="job">
+          <div className="job-info">
+            <div className="job-img">
+              <a href="">
+                {" "}
+                <img
+                  src={}
+                  className="my-2"
+                  style={{ width: "100%" }}
+                  alt=""
+                />
+              </a>
+            </div>
+            <div className="job-text">
+              <h4>Project-6</h4>
+              <p>
+              Project-6 description
+              </p>
+            </div>
+          </div>
+        </div>
+
+
+      </section>
+    </div>
+  );
+}
+
+export default Portfolio;
---
+++ src/components/resume/index.js
@@ -0,0 +1,91 @@
+function Resume() {
+  return (
+    <div>
+      <section id="welcome-section" className="download-intro">
+        <div className="flex-row">
+          <h2 className="section-title primary-border">My Resume</h2>
+        </div>
+      </section>
+
+      <section id="home-page-body" className="resume-body">
+        <div className="article column1">
+          <p className="column-title">General</p>
+          <p className="column-text">
+            <ul>
+              <li>Education: Biology Bachelors Degree</li>
+              <li>Experience: Customer representative</li>
+              <li>Experience: Planning Reporting Monitoring Specialist</li>
+              <li>Experience: Collection & Billing Analyst</li>
+              <li>Experience: Collection & Billing Team Leader</li>
+              <li>Experience: TS Budgeting & Planning Specialist</li>
+              <li>Experience: Line Cook / Chef</li>
+            </ul>
+          </p>
+        </div>
+      </section>
+
+      <section id="home-page-body" className="resume-body">
+        <div className="article column1">
+          <p className="column-title">Certificates</p>
+          <p className="column-text">
+            <ul>
+              <li>MS Excel</li>
+              <li>MS Access</li>
+              <li>Web Development</li>
+              <li>Oracle SQL Server</li>
+              <li>MS SQL Server</li>
+            </ul>
+          </p>
+        </div>
+      </section>
+
+      <section id="home-page-body" className="resume-body">
+        <div className="article column1">
+          <p className="column-title">Web Dev</p>
+          <p className="column-text">
+            <ul>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>JavaScript</li>
+              <li>Node.Js</li>
+              <li>Express.js</li>
+              <li>MySQL & Sequelize</li>
+              <li>NoSQL</li>
+              <li>Progressive Web Applications (PWA)</li>
+              <li>MongoDB & Mongoose</li>
+              <li>React</li>
+            </ul>
+          </p>
+        </div>
+      </section>
+
+      <section id="home-page-body" className="resume-body">
+        <div className="article column1">
+          <p className="column-title">Hobbies</p>
+          <p className="column-text">
+            <ul>
+              <li>Cats</li>
+              <li>Fresh Water Aquariums</li>
+              <li>Soccer</li>
+              <li>Mind Games</li>
+              <li>Video Games</li>
+              <li>Movies - SciFi, Horror, Dark Fantasy</li>
+              <li>Books</li>
+              <li>Music - Heavy Metal</li>
+              <li>Drawing </li>
+              <li>Technology</li>
+              <li>Fixing Anything Broken</li>
+              <li>History</li>
+              <li>Science</li>
+              <li>Philosophy</li>
+              <li>Fixing Anything Broken</li>
+              <li>Cooking</li>
+            </ul>
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Resume;
